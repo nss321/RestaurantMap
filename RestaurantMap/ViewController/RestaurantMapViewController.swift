a6d961f9854cf0ebcae3dafc70a52c37cdca83e6
BAE <lexon1209@gmail.com>
2025-01-08 19:52:56 +0900

    Feat: SegmentedControl을 이용한 지도 필터 구현

--- RestaurantMap/ViewController/RestaurantMapViewController.swift
+++ RestaurantMap/ViewController/RestaurantMapViewController.swift
@@ -8,13 +8,19 @@
 import UIKit
 import MapKit
 
-class RestaurantMapViewController: UIViewController {
+final class RestaurantMapViewController: UIViewController {
     
     @IBOutlet var mapView: MKMapView!
+    @IBOutlet var bigCategorySegmentedControl: UISegmentedControl!
     
-    var annotationRawData: [MKAnnotation] {
+    let food = ["한식", "일식", "중식", "양식", "분식"]
+    let cafe = ["샐러드", "카페"]
+    
+    let annotationRawData = RestaurantList().restaurantArray
+    
+    var allOfAnnotations: [MKAnnotation] {
         var annotations: [MKAnnotation] = []
-        RestaurantList().restaurantArray.forEach {
+        annotationRawData.forEach {
             let singleAnnotation = MKPointAnnotation()
             singleAnnotation.coordinate = CLLocationCoordinate2D(latitude: $0.latitude, longitude: $0.longitude)
             singleAnnotation.title = $0.name
@@ -23,26 +29,71 @@ class RestaurantMapViewController: UIViewController {
         return annotations
     }
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
         configMapView()
-        annotationRawData.forEach {
+        allOfAnnotations.forEach {
             print($0.title!)
         }
+        
+        print(makeCategorisedAnnotationList(categories: cafe, rawData: annotationRawData))
     }
 
     func configMapView() {
         let center = CLLocationCoordinate2D(latitude: 37.654105, longitude: 127.047968)
         mapView.region = MKCoordinateRegion(center: center, latitudinalMeters: 200, longitudinalMeters: 200)
-        mapView.addAnnotations(annotationRawData)
+        mapView.addAnnotations(allOfAnnotations)
     }
     
-    func makeAnnotations() {
+    @IBAction func categoryChanged(_ sender: UISegmentedControl) {
+        print(sender.selectedSegmentIndex)
+        mapView.removeAnnotations(mapView.annotations)
+        var list: [Restaurant] = []
+        var filteredAnnotations: [MKAnnotation] = []
         
+        switch sender.selectedSegmentIndex {
+        case 0:
+            mapView.addAnnotations(allOfAnnotations)
+            return
+        case 1:
+            list = makeCategorisedAnnotationList(categories: food, rawData: annotationRawData)
+            filteredAnnotations = makeFilteredAnnotations(data: list)
+            mapView.addAnnotations(filteredAnnotations)
+            return
+        case 2:
+            list = makeCategorisedAnnotationList(categories: cafe, rawData: annotationRawData)
+            filteredAnnotations = makeFilteredAnnotations(data: list)
+            mapView.addAnnotations(filteredAnnotations)
+            return
+        default:
+            return
+        }
     }
+    
+    func makeCategorisedAnnotationList(categories: [String], rawData: [Restaurant]) -> [Restaurant] {
+        
+        var categorised: [Restaurant] = []
+        
+        for item in categories {
+            let list = rawData.filter{ $0.category == item }
+            categorised.append(contentsOf: list)
+        }
+        
+        return categorised
+    }
+    
+    func makeFilteredAnnotations(data: [Restaurant]) -> [MKAnnotation] {
+        var annotations: [MKAnnotation] = []
+        data.forEach {
+            let singleAnnotation = MKPointAnnotation()
+            singleAnnotation.coordinate = CLLocationCoordinate2D(latitude: $0.latitude, longitude: $0.longitude)
+            singleAnnotation.title = $0.name
+            annotations.append(singleAnnotation)
+        }
+        return annotations
+    }
+    
 }
 
 extension RestaurantMapViewController: MKMapViewDelegate {
